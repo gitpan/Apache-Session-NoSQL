ded7cf80ec6781249c3e4c01c1ccc0e48b8effc7
Xavier Guimard <x.guimard@free.fr>
2010-09-24 10:04:02 +0000

    Import of GUIMARD/Apache-Session-NoSQL-0.03 from CPAN.
    gitpan-cpan-distribution: Apache-Session-NoSQL
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         GUIMARD/Apache-Session-NoSQL-0.03.tar.gz
    gitpan-cpan-author:       GUIMARD
    gitpan-cpan-maturity:     released

--- lib/Apache/Session/Store/NoSQL/Redis.pm
+++ lib/Apache/Session/Store/NoSQL/Redis.pm
@@ -9,31 +9,33 @@ sub new {
     my ( $class, $session ) = @_;
     my $self;
 
-    $self->{cache} = Redis->new( %{$session} );
-    print STDERR Dumper(\@_);use Data::Dumper;
+    $self->{cache} = Redis->new( %{ $session->{args} } );
 
     bless $self, $class;
 }
 
 sub insert {
     my ( $self, $session ) = @_;
-    $self->{cache}->set($session->{data}->{_session_id},$session->{serialized});
+    $self->{cache}
+      ->set( $session->{data}->{_session_id}, $session->{serialized} );
 }
 
 *update = *insert;
 
 sub materialize {
     my ( $self, $session ) = @_;
-    $self->{cache}->get($session->{data}->{_session_id}) or die 'Object does not exist in data store.';
+    $self->{cache}->get( $session->{data}->{_session_id} )
+      or die 'Object does not exist in data store.';
 }
 
 sub remove {
     my ( $self, $session ) = @_;
-    $self->{cache}->del($session->{data}->{_session_id});
+    $self->{cache}->del( $session->{data}->{_session_id} );
 }
 
 1;
 __END__
+
 =pod
 
 =head1 NAME
@@ -43,10 +45,8 @@ Apache::Session::Store::NoSQL::Redis - An implementation of Apache::Session::Sto
 =head1 SYNOPSIS
 
  use Apache::Session::NoSQL;
-
- #if you want Apache::Session to open new DB handles:
-
- tie %hash, 'Apache::Session::MySQL', $id, {
+ 
+ tie %hash, 'Apache::Session::NoSQL', $id, {
     Driver => 'Redis',
     # optional: default to localhost
     server => '127.0.0.1:6379',
